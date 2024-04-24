cb5c4b0095cdcf4decdad5d9751f8d54c849768e
azertypow <azertypow@gmail.com>
2024-04-24 22:51:18 +0200

    fix: empty html generated

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+  <title>La CARTE DE VISITE Genève</title>
+</svelte:head>
+
+<div
+        class="s-page"
+>
+  <img src="/assets/logo.svg"  alt="logo" width="auto" height="50px">
+  <p style="font-size: .75rem; line-height: 1.25em; margin-top: 2rem; margin-bottom: 2rem">Se transforme pour devenir</p>
+  <p>Atelier Tramons
+    <br>Espace associatif dédier à l'imprimerie, Genève
+    <br>☆ Impression ☆ Édition ☆ Formation ☆
+    <br>Riso 12 couleurs | Press Typo | Offset
+  </p>
+
+  <a class="app-button" href="https://tramons.ch/"
+     target="_self"
+     style="margin-top: 1rem"
+  >Accéder au site</a>
+</div>
